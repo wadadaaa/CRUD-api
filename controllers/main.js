3d4de65b1d2884e182da0b7cd5b52706959255cf
wadadaaa <lopatinski@gmail.com>
2020-03-29 18:24:55 +0300

    init

---
+++ controllers/main.js
@@ -0,0 +1,37 @@
+const getTableData = (req, res, db) => {
+  db.select('*').from('crudtable')
+    .then(items => {
+      if(items.length){
+        res.json(items)
+      } else {
+        res.json({dataExists: 'false'})
+      }
+    })
+    .catch(err => res.status(400).json({dbError: 'db error'}))
+}
+
+const postTableData = (req, res, db) => {
+  const { name, bio, fb_id } = req.body
+  db('crudtable').insert({name, bio, fb_id})
+    .returning('*')
+    .then(item => {
+      res.json(item)
+    })
+    .catch(err => res.status(400).json({dbError: 'db error'}))
+}
+
+const putTableData = (req, res, db) => {
+  const { id, name, bio, fb_id } = req.body
+  db('crudtable').where({id}).update({name, bio, fb_id})
+    .returning('*')
+    .then(item => {
+      res.json(item)
+    })
+    .catch(err => res.status(400).json({dbError: 'db error'}))
+}
+
+module.exports = {
+  getTableData,
+  postTableData,
+  putTableData,
+}
