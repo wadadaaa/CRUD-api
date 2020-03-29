2c7f3561653c622d7fe6abee737ef67477974f69
wadadaaa <lopatinski@gmail.com>
2020-03-29 19:50:48 +0300

    clean up code

--- controllers/main.js
+++ controllers/main.js
@@ -8,30 +8,30 @@ const getTableData = (req, res, db) => {
       }
     })
     .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+};
 
 const postTableData = (req, res, db) => {
-  const { name, bio, fb_id } = req.body
+  const { name, bio, fb_id } = req.body;
   db('crudtable').insert({name, bio, fb_id})
     .returning('*')
     .then(item => {
       res.json(item)
     })
     .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+};
 
 const putTableData = (req, res, db) => {
-  const { id, name, bio, fb_id } = req.body
+  const { id, name, bio, fb_id } = req.body;
   db('crudtable').where({id}).update({name, bio, fb_id})
     .returning('*')
     .then(item => {
       res.json(item)
     })
     .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+};
 
 module.exports = {
   getTableData,
   postTableData,
   putTableData,
-}
+};
